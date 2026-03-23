732094155d1df5f8463206314a8e68c14bcf2ee8
bonggyulim <bonggyulim0728@gmail.com>
2026-03-23 18:46:26 +0900

    fix: 이전 버전으로 롤백

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # 앱 시작 시 데이터베이스 테이블 자동 생성
-    from app.infrastructure.service_db.init_db import create_db_tables
-    create_db_tables()
-    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
